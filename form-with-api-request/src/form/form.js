b42c7e05c0edb7c20d0303a684578f37173ce71b
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2021-03-31 18:56:17 +0100

    Add form fields test

--- form-with-api-request/src/form/form.js
+++ form-with-api-request/src/form/form.js
@@ -1,5 +1,35 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField'
+import InputLabel from '@material-ui/core/InputLabel'
+import Select from '@material-ui/core/Select'
 
-export const Form = () => <h1>Create product</h1>
+export const Form = () => (
+  <>
+    <h1>Create product</h1>
+    {/* Material UI requires to add id to TextField since 
+    it returns labels and inputs that must be related */}
+    <form>
+      <TextField label="name" id="name" />
+
+      <TextField label="size" id="size" />
+
+      <InputLabel htmlFor="type">type</InputLabel>
+
+      <Select
+        native
+        value=""
+        inputProps={{
+          name: 'type',
+          id: 'type',
+        }}
+      >
+        <option aria-label="None" value="" />
+        <option value={'electronic'}>Electronic</option>
+        <option value={'furniture'}>Furniture</option>
+        <option value={'clothing'}>Clothing</option>
+      </Select>
+    </form>
+  </>
+)
 
 export default Form
--- form-with-api-request/src/form/form.test.js
+++ form-with-api-request/src/form/form.test.js
@@ -4,13 +4,27 @@ import {screen, render} from '@testing-library/react'
 import {Form} from './form'
 
 describe('when the form is mounted', () => {
-  // test product form page
-  it('there must be a create product form page', () => {
-    render(<Form />)
+  // beforeEach to render the component before each test
+  beforeEach(() => render(<Form />))
 
-    // getByRole with name option guarantees that you can filter the returned elements by their accessible name
+  // test form page
+  it('there must be a create product form page', () => {
+    // getByRole with name option is used to get elements by their accessible name
     expect(
       screen.getByRole('heading', {name: /create product/i}),
     ).toBeInTheDocument()
+  })
+
+  // test form fields
+  it('should exists the fields: name, size, type (electronic, furniture, clothing)', () => {
+    // getByLabelText is used to get elements by label (forms)
+    expect(screen.getByLabelText(/name/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/size/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/type/i)).toBeInTheDocument()
+
+    // queryByText is used to get elements by text
+    expect(screen.queryByText(/electronic/i)).toBeInTheDocument()
+    expect(screen.queryByText(/furniture/i)).toBeInTheDocument()
+    expect(screen.queryByText(/clothing/i)).toBeInTheDocument()
   })
 })
